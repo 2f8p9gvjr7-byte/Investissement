0b19156cbb4e173c47f32028ac44ee800be8bd66
2f8p9gvjr7-byte <2f8p9gvjr7@privaterelay.appleid.com>
2026-07-13 21:55:19 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'comparateur-v1783950000000';
+const CACHE = 'comparateur-v01';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
